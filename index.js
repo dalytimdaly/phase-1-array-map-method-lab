5dfb8c654bcf33bd98f482e1b819dc8faaee9bd5
Tim <dalytimdaly@gmail.com>
2022-08-12 09:19:47 -0600

    commit tree 6309bfad768b5c8b7f5636f21ab806ec54480468

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(tutorials) {
+  return tutorials.charAt(0).toUpperCase() + tutorials.slice(1);
 }
+
+newArr = tutorials.split(' ').map(titleCased).join(' ');
+
+
+
+
+
+console.log(titleCased(tutorials));
